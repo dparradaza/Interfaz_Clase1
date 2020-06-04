87933f82757a06f042e65ef14aee92192b28569c
dparradaza <daparrad@gmail.com>
2020-06-04 15:49:09 -0500

    crear clases

---
+++ clase1/src/app/app.java
@@ -0,0 +1,12 @@
+package app;
+
+import app.vistaPrincipal.VistaPrincipalTemplate;
+
+public class app {
+
+	public static void main(String[] args) {
+		VistaPrincipalTemplate vista = new VistaPrincipalTemplate(); 
+
+	}
+
+}
---
+++ clase1/src/app/vistaPrincipal/VistaPrincipalTemplate.java
@@ -0,0 +1,28 @@
+package app.vistaPrincipal;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class VistaPrincipalTemplate extends JFrame {
+
+    private JButton bEnviar;
+    private JTextField tNombre;
+    private JLabel lnombre;
+
+    public VistaPrincipalTemplate() {
+        super("Vista Principal");
+        this.saludar();
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(1200, 700);
+        setLocationRelativeTo(this);
+        setLayout(null);
+        setVisible(true);
+    }
+
+    public void saludar(){
+        String mensaje = "Hola Mundo";
+        System.out.println(mensaje);
+    }
+}
